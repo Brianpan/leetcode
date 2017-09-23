6230d8af2f73a3d5263f86d68ab7eb7ad4360a69
Brianpan <b97705021@ntu.edu.tw>
2017-09-23 12:37:27 -0700

    442 finish

---
+++ algorithms/442_Find_All_Duplicates_in_an_Array.py
@@ -0,0 +1,22 @@
+#---------Tags---------
+#Array
+#----------------------
+
+#---------Notes---------
+# Trick to use minus to note the status
+#-----------------------
+
+class Solution(object):
+    def findDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """        
+        res = []
+        for n in nums:
+            if nums[abs(n) - 1] < 0:
+                res.append(abs(n))
+            else:
+                nums[abs(n)-1] *= -1
+        
+        return res
